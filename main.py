3d8776799af8e0e1c080a38cdb6bab292f2049f4
Olek <aleksanderlis92@gmail.com>
2021-01-08 14:39:25 +0100

    1

--- main.py
+++ main.py
@@ -6,17 +6,19 @@ from input_handlers import EventHandler
 
 
 def main() -> None:
+    #Okno gry i zestaw elementow
     screen_width = 80
     screen_height = 50
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+    #Zmienne pozycja rogue
     player_x = int(screen_width / 2)
     player_y = int(screen_height / 2)
 
     event_handler = EventHandler()
-
+    #nowe okno
     with tcod.context.new_terminal(
             screen_width,
             screen_height,
@@ -24,6 +26,7 @@ def main() -> None:
             title="Yet Another Roguelike Tutorial",
             vsync=True,
     ) as context:
+        #okno x i y F odwraca
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
             root_console.print(x=player_x, y=player_y, string="@")
@@ -36,11 +39,11 @@ def main() -> None:
 
                 if action is None:
                     continue
-
+                #akcja ruchu
                 if isinstance(action, MovementAction):
                     player_x += action.dx
                     player_y += action.dy
-
+                #akcja escape
                 elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
